a5dd56be4a56e625ce461dc68e9cb829210db5c8
Benjamin Desmoulins <benjamin.desmoulins@gmail.com>
2022-06-03 10:47:01 +0200

    Program simplification

--- books-online.py
+++ books-online.py
@@ -67,62 +67,62 @@ Créer les liste suivantes :
 """
 
 #Pour chaque URL de liste livre, utiliser BeautifulSoup pour extraire les données et les stocker dans les listes
-
 tableau_livre = []
 url_livre = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 html_livre = requests.get(url_livre).text
 soup_livre = BeautifulSoup(html_livre, 'html.parser')
 
+#URL Livre 
+product_page_url = url_livre
+
 #Title
-title_livre = soup_livre.find(class_="col-sm-6 product_main")
-title_livre = title_livre.find('h1').string
+title = soup_livre.find(class_="col-sm-6 product_main")
+title = title.find('h1').string
 
 #Category
 for a_href in soup_livre.find_all("a"):
     list_a = []
     list_a.append(a_href.string)
-    #print(a_href.string)
 category = list_a[-1]
 
 #Description
 description_class_search = soup_livre.find(class_="product_page")
 children_product_page = description_class_search.findChildren('p', recursive = False)
 description_livre_clean_tag = str(children_product_page).replace("<p>", "")
-description_livre = str(description_livre_clean_tag).replace("</p>", "")
+product_description = str(description_livre_clean_tag).replace("</p>", "")
 
-#Review_rating TODO : Retirer les balises <p class='star-rating ...>
+#Review_rating 
 review_livre = soup_livre.find(class_="col-sm-6 product_main")
 children_review_livre = review_livre.find(class_="star-rating Three", recursive = False)
 children_review_livre.clear()
-print(children_review_livre)
+children_review_livre_2 = str(children_review_livre).replace('<p class="star-rating ', "")
+review_rating = children_review_livre_2.replace('"></p>', "")
 
-#Extraction des données du tableau
+#Extraction des données du tableau (UPC, Price excluding taxe, Price including tax, Number available)
 for td in soup_livre.find_all('td'):
     tableau_livre.append(td.string)
-
-# universal_product_code(tableau_livre[0])
-# price_excluding_tax(tableau_livre[2])
-# price_including_tax(tableau_livre[3])
-# number_available(tableau_livre[5])
+universal_product_code = tableau_livre[0]
+price_excluding_tax = tableau_livre[2]
+price_including_tax = tableau_livre[3]
+number_available = tableau_livre[5]
 
 #Extraction de l'URL de l'image
 image_livre = soup.select("div img")
-url_image_livre = url_website + image_livre[0]["src"]
-download_image = requests.get(url_image_livre).content
+image_url = url_website + image_livre[0]["src"]
+download_image = requests.get(image_url).content
 with open("test_img.jpg", "wb") as handler:
     handler.write(download_image)
 
 #Créer une liste avec les entêtes suivantes : [”product_page_url”, “universal_ product_code (upc)”, “title, price_including_tax”, “price_excluding_tax”, “product_description”, “category”, “review_rating”, “image_url”]
 liste_csv = []
-liste_entetes = ["product_page_url", "universal_ product_code (upc)", "title", "price_including_tax", "price_excluding_tax", "product_description", "category", "review_rating", "image_url"]
-# infos_livre = [product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax, product_description, category, review_rating, image_url]
-liste_livre = [title_livre]
+entetes = ["product_page_url", "universal_product_code (upc)", "title", "price_including_tax", "price_excluding_tax", "product_description", "category", "review_rating", "image_url"]
+informations_livre = [product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax, product_description, category, review_rating, image_url]
 
 #Ouvrir un fichier CSV et y importer les informations dans des colonnes différentes
 with open('livre_info.csv', 'w', encoding='UTF-8') as f:
     writer = csv.writer(f)
-    liste_csv.append(liste_entetes)
-    liste_csv.append(liste_livre)
+    liste_csv.append(entetes)
+    liste_csv.append(informations_livre)
     writer.writerows(liste_csv)
 #Enregistrer et fermer le fichier CSV
 
